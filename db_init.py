43f67026e58ad24c4bfb0d966d16eeaf5f2a90cd
bmircea23 <bmircea23@gmail.com>
2020-11-16 13:33:25 +0200

    DB Initialization

---
+++ db_init.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+
+#Create database file
+open("database.db", 'a').close()
+
+#Create connection and cursor
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+#Table creation queries
+query_list = ["CREATE TABLE available (date date)",
+              "CREATE TABLE listeners (ID text, wdate date)"]
+
+#Execute queries
+for query in query_list:
+    cursor.execute(query)
+
+#Never forget to commit
+connection.commit()
+
+#Close connection
+connection.close()
